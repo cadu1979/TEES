e8aa7c5628d69ea6344705cceb7fe32d26135dc5
cadu1979 <43002128+cadu1979@users.noreply.github.com>
2022-03-06 00:21:24 -0300

    Add files via upload

---
+++ doxygen/html/search/all_3.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['print_5fheader_0',['print_header',['../eleicoes_8php.html#a445389ddfffe227dbc6d8add234ced18',1,'eleicoes.php']]],
+  ['print_5ftable_5fhead_5fprefeitos_1',['print_table_head_prefeitos',['../eleicoes_8php.html#a6b61e415ec075133f0c4cf246c71c2d6',1,'eleicoes.php']]],
+  ['print_5ftable_5fhead_5fvereadores_2',['print_table_head_vereadores',['../eleicoes_8php.html#aca392b2f53056eeb40bf85ca439e6bd7',1,'eleicoes.php']]],
+  ['print_5ftable_5fprefeitos_3',['print_table_prefeitos',['../eleicoes_8php.html#ac7726cc56f4f82a2924c612d0bf5925e',1,'eleicoes.php']]],
+  ['print_5ftable_5fvereadores_4',['print_table_vereadores',['../eleicoes_8php.html#afcfa9d6429c0e14468c40570d8804c71',1,'eleicoes.php']]],
+  ['print_5fvencedor_5fprefeito_5',['print_vencedor_prefeito',['../eleicoes_8php.html#aa4821fa04ee8ff536e24eb0ccdad5102',1,'eleicoes.php']]],
+  ['print_5fvencedor_5fvereador_6',['print_vencedor_vereador',['../eleicoes_8php.html#a2857745f6d130b0fd68ceb07db7472ae',1,'eleicoes.php']]]
+];
---
+++ doxygen/html/search/variables_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['foreach_0',['foreach',['../eleicoes_8php.html#a7b5ab40907cfd960efcd5bf3d89e863b',1,'eleicoes.php']]]
+];
